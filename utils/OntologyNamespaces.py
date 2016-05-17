50b1b59b41a3b074aa1c7edee72940098ffe4d28
albert <albert.suarez.molgo@est.fib.upc.edu>
2016-05-17 23:36:08 +0200

    change to ECSDI namespace

--- utils/OntologyNamespaces.py
+++ utils/OntologyNamespaces.py
@@ -31,3 +31,6 @@ DSO = Namespace('http://www.semanticweb.org/directory-service-ontology#')
 
 # Custom agent messaging ontology
 AMO = Namespace('http://www.semanticweb.org/houcros/ontologies/2015/4/agentsMessages')
+
+# Own namespace
+ECSDI = Namespace("http://www.owl-ontologies.com/ECSDIAmazon.owl#")
