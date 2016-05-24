a382cac3231048ea6fc7d2cece5f9e34a5afcdf8
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-05-24 19:02:38 +0200

    start cerca

---
+++ agents/UserPersonalAgent.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+filename: UserPersonalAgent
+
+Agent que implementa la interacció amb l'usuari
+
+
+@author: casassg
+"""
+
+__author__ = 'casassg'
+
+import argparse
+import socket
+from multiprocessing import Process
+
+from flask import Flask, render_template, request
+from rdflib import Graph, Namespace
+
+from utils.Agent import Agent
+from utils.FlaskServer import shutdown_server
+from utils.Logging import config_logger
+
+# Definimos los parametros de la linea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--open', help="Define si el servidor est abierto al exterior o no", action='store_true',
+                    default=False)
+parser.add_argument('--port', type=int, help="Puerto de comunicacion del agente")
+parser.add_argument('--dhost', default=socket.gethostname(), help="Host del agente de directorio")
+parser.add_argument('--dport', type=int, help="Puerto de comunicacion del agente de directorio")
+
+# Logging
+logger = config_logger(level=1)
+
+# parsing de los parametros de la linea de comandos
+args = parser.parse_args()
+
+# Configuration stuff
+if args.port is None:
+    port = 9002
+else:
+    port = args.port
+
+if args.open is None:
+    hostname = '0.0.0.0'
+else:
+    hostname = socket.gethostname()
+
+if args.dport is None:
+    dport = 9000
+else:
+    dport = args.dport
+
+if args.dhost is None:
+    dhostname = socket.gethostname()
+else:
+    dhostname = args.dhost
+
+# Flask stuff
+app = Flask(__name__, template_folder='../templates')
+
+# Configuration constants and variables
+agn = Namespace("http://www.agentes.org#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+AgentePersonal = Agent('UserPersonalAgent',
+                       agn.UserPersonalAgent,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('DirectoryAgent',
+                       agn.Directory,
+                       'http://%s:%d/Register' % (dhostname, dport),
+                       'http://%s:%d/Stop' % (dhostname, dport))
+
+# Global dsgraph triplestore
+dsgraph = Graph()
+
+
+@app.route("/cerca", methods=['GET', 'POST'])
+def browser_cerca():
+    """
+    Permite la comunicacion con el agente via un navegador
+    via un formulario
+    """
+    if request.method == 'GET':
+        return render_template('cerca.html')
+    else:
+        user = request.form['username']
+        mess = request.form['message']
+        return render_template('cerca.html', user=user, mess=mess)
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion del agente
+    """
+    return "Hola"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+
+
+def agentbehavior1():
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1)
+    ab1.start()
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    logger.info('The End')
